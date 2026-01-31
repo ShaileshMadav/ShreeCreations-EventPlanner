1bca15c956641c4457c020dd6ff979df67fc8758
ShaileshMadav <madavshailesh@gmail.com>
2026-01-31 12:40:07 +0530

    Fix media URLs for production backend

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
 // middlewares
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }),
+);
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/bookings", bookingRoutes);
--- frontend/src/components/PlanCard.jsx
+++ frontend/src/components/PlanCard.jsx
@@ -1,11 +1,12 @@
+const backendBaseUrl = import.meta.env.VITE_API_URL.replace("/api", "");
 const PlanCard = ({ plan, onBook }) => {
   return (
     <div className="plan-card">
       <div className="media">
         {plan.mediaType === "image" ? (
-          <img src={`http://localhost:5000${plan.mediaUrl}`} alt={plan.name} />
+          <img src={`${backendBaseUrl}${plan.mediaUrl}`} alt={plan.name} />
         ) : (
-          <video src={`http://localhost:5000${plan.mediaUrl}`} controls />
+          <video src={`${backendBaseUrl}${plan.mediaUrl}`} controls />
         )}
         {plan.offer && (
           <span className="offer-badge" hidden>
